1d46ced8b2c68711b32802514769faab55da25da
hai123vn <haipmbdvn@gmail.com>
2023-03-11 12:58:07 +0700

    Hoàn thành các chức năng Shift Data Maintenance

---
+++ API/Models/MS_Location.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class MS_Location
+    {
+        [Key]
+        public string Manuf {get; set;}
+        [Key]
+        public string StoreH {get; set;}
+        public string LocationName {get; set;}
+    }
+}
