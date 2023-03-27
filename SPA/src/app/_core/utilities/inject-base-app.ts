34b611e9ff2776e69f0025fd8cc8e3581dbe99b7
phu.tran <trantrongphu06102000@gmail.com>
2023-03-27 10:15:40 +0700

    update error inject base comment

--- SPA/src/app/_core/utilities/inject-base-app.ts
+++ SPA/src/app/_core/utilities/inject-base-app.ts
@@ -2,13 +2,7 @@ import { inject } from "@angular/core";
 import {ActivatedRoute, Router} from "@angular/router";
 // import { DestroyService } from "@services/common/destroy.service";
 // import { NgSnotifyService } from '@services/common/ng-snotify.service';
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
->>>>>>> 5e7b9dd (1.3 Department Data Maintenance (Done))
-=======
->>>>>>> 491dbe8 (demo1)
 import { FunctionUtility } from "@utilities/function-utility";
 import { NgxSpinnerService } from "ngx-spinner";
 export abstract class InjectBase {
